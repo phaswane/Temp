1b09c3bbf7390c34c4dbb905ccde132b1661b028
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-07-23 20:35:23 +0200

    Header file

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 1024
+#define MAX_ARGS 64
+
+char* read_input();
+char** tokenize_input(char* input);
+int execute_command(char **args);
+int handle_builtin(char **args);
+
+#endif
